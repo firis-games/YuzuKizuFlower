23c60b2b64f1f699cea6e1aa77a471352ef1f6a8
firis <firis.games.lapismoonlight@gmail.com>
2019-02-24 23:35:22 +0900

    アカリカルチャー関連のレシピを追加

--- src/main/java/firis/yuzukizuflower/common/recipe/BotaniaRecipes.java
+++ src/main/java/firis/yuzukizuflower/common/recipe/BotaniaRecipes.java
@@ -1,5 +1,6 @@
 package firis.yuzukizuflower.common.recipe;
 
+import firis.yuzukizuflower.YuzuKizuFlower.YuzuKizuItems;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
@@ -8,6 +9,27 @@ import vazkii.botania.api.BotaniaAPI;
 import vazkii.botania.api.recipe.RecipeManaInfusion;
 
 public class BotaniaRecipes {
+	
+	/**
+	 * botania花びらの鉱石辞書
+	 */
+	public static final String[] OREDICT_PETAL = new String[] {
+			"petalWhite", "petalOrange", "petalMagenta", "petalLightBlue",
+			"petalYellow", "petalLime", "petalPink", "petalGray",
+			"petalLightGray", "petalCyan", "petalPurple", "petalBlue",
+			"petalBrown", "petalGreen", "petalRed", "petalBlack"
+	};
+	
+	/**
+	 * botaniaルーンの鉱石辞書
+	 */
+	public static final String[] RUNE = new String[] {
+			"runeWaterB", "runeFireB", "runeEarthB", "runeAirB",
+			"runeSpringB", "runeSummerB", "runeAutumnB", "runeWinterB",
+			"runeManaB", "runeLustB", "runeGluttonyB", "runeGreedB",
+			"runeSlothB", "runeWrathB", "runeEnvyB", "runePrideB"
+	};
+	
 	
 	/**
 	 * botaniaのレシピを追加する
@@ -19,6 +41,10 @@ public class BotaniaRecipes {
 		
 		//オアキド変換レシピ
 		BotaniaRecipes.addRecipesOrechild();
+		
+		//ペタルアポセカリー変換レシピ
+		BotaniaRecipes.addRecipesPetalApothecary();
+		
 		
 	}
 	
@@ -76,6 +102,20 @@ public class BotaniaRecipes {
 	protected static void addRecipesOrechild() {
 		//グロウストーン
 		BotaniaAPI.addOreWeightNether("glowstone", 4900); // Vanilla
+	}
+	
+	/**
+	 * ペタルアポセカリー変換レシピを追加する
+	 */
+	protected static void addRecipesPetalApothecary() {
+		//アカリカルチャー
+		BotaniaAPI.registerPetalRecipe(
+				new ItemStack(YuzuKizuItems.AKARICULTURE), 
+				OREDICT_PETAL[1], 
+				OREDICT_PETAL[1],
+				OREDICT_PETAL[10],
+				RUNE[2],
+				"redstoneRoot");
 	}
 	
 
