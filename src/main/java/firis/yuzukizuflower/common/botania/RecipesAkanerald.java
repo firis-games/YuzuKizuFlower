72b7ced124f0fe7815b5819fa3d4809d0737fee3
firis <firis.games.lapismoonlight@gmail.com>
2019-03-10 23:52:32 +0900

    箱入りアカネラルドの生成処理追加

--- src/main/java/firis/yuzukizuflower/common/botania/RecipesAkanerald.java
+++ src/main/java/firis/yuzukizuflower/common/botania/RecipesAkanerald.java
@@ -1,8 +1,20 @@
 package firis.yuzukizuflower.common.botania;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import javax.annotation.Nonnull;
 
+import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.village.MerchantRecipe;
+import net.minecraft.village.MerchantRecipeList;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.registry.VillagerRegistry;
+import net.minecraftforge.fml.common.registry.VillagerRegistry.VillagerProfession;
 
 /**
  * アカネラルドのレシピ
@@ -10,6 +22,8 @@ import net.minecraft.item.ItemStack;
  *
  */
 public class RecipesAkanerald implements IManaRecipes {
+	
+	private static Random rand = new Random();
 	
 	/**
 	 * 一致するレシピを検索する
@@ -20,7 +34,79 @@ public class RecipesAkanerald implements IManaRecipes {
 		
 		ManaRecipe recipe = null;
 		
+		//20000mana 40tick
+		recipe = new ManaRecipe(
+				stack,
+				createVillagerItem(simulate),
+				20000, 40);
+		
 		return recipe;
+	}
+	
+	private ItemStack createVillagerItem(boolean simulate) {
+		
+		//シミュレートonの場合は何もしない
+		if (simulate) {
+			return ItemStack.EMPTY.copy();
+		}
+		
+		ItemStack result = ItemStack.EMPTY.copy();
+		List<ItemStack> stackList = populateBuyingList();
+		int idx = rand.nextInt(stackList.size());
+		result = stackList.get(idx);
+		return result;
+		
+	}
+	
+	/**
+	 * 村人のトレードアイテムを生成する
+	 */
+	@SuppressWarnings("deprecation")
+	private List<ItemStack> populateBuyingList() {
+		
+		//村人のトレードアイテムリスト
+		MerchantRecipeList buyingList = new MerchantRecipeList();
+		
+		//職業をランダムで取得
+		int careerId = VillagerRegistry.FARMER.getRandomCareer(rand);
+		
+		//全レベルの交換アイテムを取得する
+		List<EntityVillager.ITradeList> trades = new ArrayList<EntityVillager.ITradeList>();
+		//村人タイプ
+		VillagerProfession prof = VillagerRegistry.getById(careerId);
+		//村人タイプの種類
+		int profCareer = prof.getRandomCareer(rand);
+		int level = 0;
+		while (true) {
+			List<EntityVillager.ITradeList> work = prof.getCareer(profCareer).getTrades(level);
+			if (work != null) {
+				trades.addAll(work);
+				level += 1;
+			} else {
+				break;
+			}
+		}
+		
+		//交換レシピをMerchantRecipeListへ格納する
+		World world = FMLCommonHandler.instance().getMinecraftServerInstance().getEntityWorld();
+		for (EntityVillager.ITradeList list : trades) {
+			list.addMerchantRecipe(new EntityVillager(world), buyingList, rand);
+		}
+		
+		//エメラルド以外を取得
+		List<ItemStack> result = new ArrayList<ItemStack>();
+		for (MerchantRecipe recipe : buyingList) {
+			if (!recipe.getItemToSell().getItem().equals(Items.EMERALD)) {
+				result.add(recipe.getItemToSell());
+			}
+		}
+		
+		//再帰処理
+		if (result.size() == 0) {
+			result = populateBuyingList();
+		}
+		
+		return result;
 	}
 	
 }
