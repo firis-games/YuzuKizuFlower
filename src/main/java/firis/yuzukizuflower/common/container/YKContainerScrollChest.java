a0c2d6b8032c1796b1465d15bfb68e81b777d75b
firis <firis.games.lapismoonlight@gmail.com>
2019-04-18 21:33:13 +0900

    スクロールチェストのアイテム移動同期修正
    ・スクロールチェストのアイテム移動時に同期処理を行うように修正

--- src/main/java/firis/yuzukizuflower/common/container/YKContainerScrollChest.java
+++ src/main/java/firis/yuzukizuflower/common/container/YKContainerScrollChest.java
@@ -1,12 +1,33 @@
 package firis.yuzukizuflower.common.container;
 
 import firis.yuzukizuflower.common.inventory.IScrollInventory;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.item.ItemStack;
 
 public class YKContainerScrollChest extends YKContainerBaseScrollInventory {
 
 	public YKContainerScrollChest(IScrollInventory iinv, InventoryPlayer playerInv) {
 		super(iinv, playerInv);
 	}
+	
+	@Override
+	public ItemStack transferStackInSlot(EntityPlayer playerIn, int index)
+    {
+		//Clientと同期する
+		ItemStack stack = super.transferStackInSlot(playerIn, index);
+		this.detectAndSendChanges();
+		if (playerIn instanceof EntityPlayerMP) {
+			EntityPlayerMP playermp = (EntityPlayerMP) playerIn;
+			playermp.sendContainerToPlayer(this);
+		}
+		
+		return stack;
+    }
+	
+	
+	
+			
 }
 
