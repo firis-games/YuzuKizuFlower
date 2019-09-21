d90afe0da5d486b5c8b0c4f3d6b73108476c64b9
firis <firis.games.lapismoonlight@gmail.com>
2019-09-21 17:12:21 +0900

    アルフヘイムの王鍵のレアリティ表示変更
    ・レリックと同じレアリティ表示に変更
    ・通常の鍵はレアリティ・レアに変更

--- src/main/java/firis/yuzukizuflower/common/item/YKItemDimensionKey.java
+++ src/main/java/firis/yuzukizuflower/common/item/YKItemDimensionKey.java
@@ -2,6 +2,7 @@ package firis.yuzukizuflower.common.item;
 
 import java.util.List;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import firis.yuzukizuflower.YuzuKizuFlower;
@@ -13,6 +14,7 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.EnumRarity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
@@ -26,6 +28,7 @@ import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import vazkii.botania.api.BotaniaAPI;
 import vazkii.botania.api.state.BotaniaStateProps;
 import vazkii.botania.api.state.enums.AlfPortalState;
 import vazkii.botania.common.block.ModBlocks;
@@ -200,5 +203,15 @@ public class YKItemDimensionKey extends Item {
 		}
     }
 	
+	
+	@Nonnull
+	@Override
+	public EnumRarity getRarity(ItemStack stack) {
+		
+		if (this.metadata == 1) {
+			return BotaniaAPI.rarityRelic;
+		}
+		return EnumRarity.RARE;		
+	}
 	
 }
